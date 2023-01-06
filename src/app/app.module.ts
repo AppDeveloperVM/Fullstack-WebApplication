09baca28dbdec5d92cf5e7b210ae1e1762e57830
AppDeveloperVM <vicmr1994@gmail.com>
2023-01-06 11:29:32 +0100

    First Commit - basic app MVC
    - Bootstrap
    - MVC

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
+import { TarjetaCreditoComponent } from './components/tarjetas/tarjeta-credito/tarjeta-credito.component';
+import { ListTarjetaCreditoComponent } from './components/tarjetas/list-tarjeta-credito/list-tarjeta-credito.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TarjetasComponent,
+    TarjetaCreditoComponent,
+    ListTarjetaCreditoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
