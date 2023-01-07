d71f8cc8a8f860efa540ebd396562cec2f28a4b9
AppDeveloperVM <vicmr1994@gmail.com>
2023-01-07 22:10:23 +0100

    Updated tarjeta Service - add, update, delete

--- src/app/services/tarjeta.service.ts
+++ src/app/services/tarjeta.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { TarjetaCredito } from '../models/tarjetaCredito';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +10,7 @@ export class TarjetaService {
   myAppUrl = 'https://localhost:44361/';
   myApiUrl = 'api/TarjetaCredito/';
   list: TarjetaCredito[] = [];
+  private datosEdicion = new BehaviorSubject<TarjetaCredito>({} as any);
 
   constructor(private http: HttpClient) { }
 
@@ -22,5 +23,21 @@ export class TarjetaService {
     .then(data => {
       this.list = data as TarjetaCredito[];
     })
+  }
+
+  addDatosTarjetaToForm(tarjeta : TarjetaCredito){
+    this.datosEdicion.next(tarjeta);
+  }
+
+  obtenerTarjeta$() : Observable<TarjetaCredito>{
+    return this.datosEdicion.asObservable();
+  }
+
+  actualizarTarjeta(id: number, tarjeta: TarjetaCredito) : Observable<TarjetaCredito> {
+    return this.http.put<TarjetaCredito>(this.myAppUrl + this.myApiUrl + id, tarjeta);
+  }
+
+  eliminarTarjeta(id: number) : Observable<TarjetaCredito> {
+    return this.http.delete<TarjetaCredito>(this.myAppUrl + this.myApiUrl+ id);
   }
 }
